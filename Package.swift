29158f0cb7f4700dc670159443ffa238875080d8
Anthony Michalek <ianthisawesomee@gmail.com>
2024-03-04 18:32:07 -0500

    Update

--- Package.swift
+++ Package.swift
@@ -6,8 +6,13 @@ import PackageDescription
 let package = Package(
     name: "Game",
     platforms: [
+        .iOS(.v17),
         .macOS(.v14),
-        .visionOS(.v1)
+        .tvOS(.v17),
+        .macCatalyst(.v17),
+        .driverKit(.v23),
+        .visionOS(.v1),
+        .watchOS(.v10)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -16,7 +21,7 @@ let package = Package(
                  targets: ["Game"])
     ],
     dependencies: [
-        .package(url: "https://github.com/multijam/SwiftGodot", revision: "d80f53a47ed6f3a8f062ee2685823d013e178586")
+        .package(url: "https://github.com/migueldeicaza/SwiftGodot", revision: "caec7dfc054880a2aa1c43a2b2f60956242a7424")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
