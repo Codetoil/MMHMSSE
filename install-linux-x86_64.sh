1af4005fa83441ca235cb4b8e1252fcc873d5b07
Anthony Michalek <ianthisawesomee@gmail.com>
2024-03-13 10:52:13 -0400

    Actually do that

--- install-linux-x86_64.sh
+++ install-linux-x86_64.sh
@@ -4,8 +4,9 @@
 wget https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_linux.x86_64.zip
 unzip Godot_v4.2.1-stable_linux.x86_64.zip
 wget https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_export_templates.tpz
-mkdir -p ~/.local/share/godot/export_templates/4.2.1.stable
-unzip Godot_v4.2.1-stable_export_templates.tpz -d ~/.local/share/godot/export_templates/4.2.1.stable
+mkdir -p ~/.local/share/godot/export_templates/
+unzip Godot_v4.2.1-stable_export_templates.tpz -d ~/.local/share/godot/export_templates
+mv ~/.local/share/godot/export_templates/templates ~/.local/share/godot/export_templates/4.2.1.stable
 mkdir -p Godot/bin/debug/Linux/x86_64
 mkdir -p Godot/bin/release/Linux/x86_64
 mkdir -p Godot/addons/game_swift/debug/Linux/x86_64
