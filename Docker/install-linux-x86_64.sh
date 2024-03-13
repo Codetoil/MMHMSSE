f3c5f0757c7e4e155b8e76e0660f7b3a3e807da2
Anthony Michalek <ianthisawesomee@gmail.com>
2024-03-13 13:20:55 -0400

    Docker changes 2

--- Docker/install-linux-x86_64.sh
+++ Docker/install-linux-x86_64.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Run this on x86_64 Linux
 
-git clone https://github.com/Codetoil/Game.git .
+git init
+git remote add origin https://github.com/Codetoil/Game.git
+git pull origin master
 wget https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_linux.x86_64.zip
 unzip Godot_v4.2.1-stable_linux.x86_64.zip
 wget https://github.com/godotengine/godot/releases/download/4.2.1-stable/Godot_v4.2.1-stable_export_templates.tpz
